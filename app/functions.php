fc45e9d3fb51209601e36d54f14870aaf78c948c
Ray Martino <raymardesign@gmail.com>
2016-08-04 20:42:17 -0400

    Reverse order of stylesheets
    Resolves CSS specificity issue between bootstrap and underscores where
    bootstrap's _reboot was overriding the custom defined fonts in
    underscores.
    
    This illustrates the need to merge bootstrap into underscores, or
    eliminate the CSS in-fighting between them at some point.

--- app/functions.php
+++ app/functions.php
@@ -102,13 +102,14 @@ add_action( 'widgets_init', 'juliaday_widgets_init' );
  * Enqueue scripts and styles.
  */
 function juliaday_scripts() {
-	wp_enqueue_style( 'juliaday-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'juliaday-font-awesome', get_template_directory_uri() . '/font-awesome.css' );
 
     wp_enqueue_style( 'juliaday-google-fonts', 'https://fonts.googleapis.com/css?family=Handlee|Droid+Serif:400,400italic,700,700italic' );
 
 	wp_enqueue_style( 'juliaday-bootstrap-css', get_template_directory_uri() . '/bootstrap.css' );
 
-	wp_enqueue_style( 'juliaday-font-awesome', get_template_directory_uri() . '/font-awesome.css' );
+    wp_enqueue_style( 'juliaday-style', get_stylesheet_uri() );
+
 
 	wp_enqueue_script( 'juliaday-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
