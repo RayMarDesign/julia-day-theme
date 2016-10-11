2c4398ace26275cc2f0295f9de38ef08d62cacf8
Ray Martino <raymardesign@gmail.com>
2016-10-10 22:02:45 -0400

    Add post excerpt to page

--- app/template-parts/content.php
+++ app/template-parts/content.php
@@ -17,6 +17,12 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+        
+        if ( has_excerpt( $post->ID ) ) {
+            echo '<div class="deck">';
+            echo '<p>' . get_the_excerpt() . '</p>';
+            echo '</div><!-- .deck -->';
+        }
 
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
