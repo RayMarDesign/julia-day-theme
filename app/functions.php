3dcfeabfb9d958509a5b3cf96e1d7a613b5a2c0e
Ray Martino <raymardesign@gmail.com>
2016-10-29 15:14:09 -0400

    Add thumbnail sizes

--- app/functions.php
+++ app/functions.php
@@ -41,6 +41,8 @@ function juliaday_setup() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 810, 360, true );
+    add_image_size( 'juliaday-small-thumb', 200, 100 );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
