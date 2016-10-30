39895bbf930fc1678341fcb1eb67ce3b08271593
Ray Martino <raymardesign@gmail.com>
2016-10-29 22:32:23 -0400

    Update content.php for use with index.php

--- app/inc/template-tags.php
+++ app/inc/template-tags.php
@@ -56,6 +56,50 @@ function juliaday_posted_on() {
 }
 endif;
 
+if ( ! function_exists( 'juliaday_index_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author on index pages.
+ */
+function juliaday_index_posted_on() {
+    $author_id = get_the_author_meta( 'ID' );
+    
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		esc_html( get_the_modified_date() )
+	);
+
+	$byline = sprintf(
+		esc_html_x( 'by %s', 'post author', 'juliaday' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( $author_id ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+
+	$posted_on = sprintf(
+		esc_html_x( 'Published %s', 'post date', 'juliaday' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+    echo '<div class="meta-content">';
+	echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+
+	if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		/* translators: %s: post title */
+		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'juliaday' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+		echo '</span>';
+	}
+    echo '</div><!-- .meta-content -->';
+
+}
+endif;
+
+
 if ( ! function_exists( 'juliaday_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.
@@ -186,10 +230,19 @@ function juliaday_validate_gravatar($id_or_email) {
 		}
 	    wp_cache_set($hashkey, $data, $group = '', $expire = 60*5);
 
-	}		
+	}
 	if ($data == '200'){
 		return true;
 	} else {
 		return false;
 	}
 }
+
+
+/**
+ *  Customize the excerpt read-more indicator
+ */
+function juliaday_excerpt_more( $more ) {
+    return " …";
+}
+add_filter( 'excerpt_more', 'juliaday_excerpt_more' );
--- app/template-parts/content.php
+++ app/template-parts/content.php
@@ -22,35 +22,46 @@
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h2 class="entry-title index-excerpt"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
         
-        if ( has_excerpt( $post->ID ) ) {
+        if ( has_excerpt( $post->ID ) && is_single() ) {
             echo '<div class="deck">';
             echo '<p>' . get_the_excerpt() . '</p>';
             echo '</div><!-- .deck -->';
         }
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php juliaday_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		if ( 'post' === get_post_type() ) {
+			if ( is_single() ) {
+                echo '<div class="entry-meta">';
+                juliaday_posted_on();
+                echo '</div><!-- .entry-meta -->';
+            }
+            else {
+                echo '<div class="index-entry-meta">';
+                juliaday_index_posted_on();
+                echo '</div><!-- .index-entry-meta -->';
+            }
+		} ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'juliaday' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'juliaday' ),
-				'after'  => '</div>',
-			) );
+            if ( is_single() ) {
+                the_content( sprintf(
+                    /* translators: %s: Name of current post. */
+                    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'juliaday' ), array( 'span' => array( 'class' => array() ) ) ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false )
+                ) );
+                
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'juliaday' ),
+                    'after'  => '</div>',
+                ) );
+            }
+            else {
+                the_excerpt();
+            }
 		?>
 	</div><!-- .entry-content -->
 
